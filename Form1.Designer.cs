2462e4ef82764a84283bc6f882bc85bc40be781f
Sas Iacob <sasiacobconstantin@yahoo.com>
2021-02-15 23:09:38 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace XORbp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.X0outLabel = new System.Windows.Forms.Label();
+            this.X1outLabel = new System.Windows.Forms.Label();
+            this.H0outLabel = new System.Windows.Forms.Label();
+            this.X1W1_label = new System.Windows.Forms.Label();
+            this.YoutLabel = new System.Windows.Forms.Label();
+            this.X0W0_label = new System.Windows.Forms.Label();
+            this.X1W0_label = new System.Windows.Forms.Label();
+            this.H1W0_Label = new System.Windows.Forms.Label();
+            this.X0W1_label = new System.Windows.Forms.Label();
+            this.H0W0_Label = new System.Windows.Forms.Label();
+            this.H1outLabel = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Prag_H0 = new System.Windows.Forms.Label();
+            this.Prag_H1 = new System.Windows.Forms.Label();
+            this.Prag_Y = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.panel1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = global::XORbp.Properties.Resources.grafic;
+            this.panel1.Location = new System.Drawing.Point(4, 2);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(895, 455);
+            this.panel1.TabIndex = 2;
+            this.panel1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 507);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Eroare:";
+            // 
+            // X0outLabel
+            // 
+            this.X0outLabel.AutoSize = true;
+            this.X0outLabel.Location = new System.Drawing.Point(104, 102);
+            this.X0outLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.X0outLabel.Name = "X0outLabel";
+            this.X0outLabel.Size = new System.Drawing.Size(35, 13);
+            this.X0outLabel.TabIndex = 5;
+            this.X0outLabel.Text = "label2";
+          
+            // 
+            // X1outLabel
+            // 
+            this.X1outLabel.AutoSize = true;
+            this.X1outLabel.Location = new System.Drawing.Point(104, 367);
+            this.X1outLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.X1outLabel.Name = "X1outLabel";
+            this.X1outLabel.Size = new System.Drawing.Size(35, 13);
+            this.X1outLabel.TabIndex = 6;
+            this.X1outLabel.Text = "label2";
+           
+            // 
+            // H0outLabel
+            // 
+            this.H0outLabel.AutoSize = true;
+            this.H0outLabel.Location = new System.Drawing.Point(418, 102);
+            this.H0outLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.H0outLabel.Name = "H0outLabel";
+            this.H0outLabel.Size = new System.Drawing.Size(35, 13);
+            this.H0outLabel.TabIndex = 7;
+            this.H0outLabel.Text = "label2";
+          
+            // 
+            // X1W1_label
+            // 
+            this.X1W1_label.AutoSize = true;
+            this.X1W1_label.Location = new System.Drawing.Point(265, 390);
+            this.X1W1_label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.X1W1_label.Name = "X1W1_label";
+            this.X1W1_label.Size = new System.Drawing.Size(35, 13);
+            this.X1W1_label.TabIndex = 8;
+            this.X1W1_label.Text = "label2";
+            // 
+            // YoutLabel
+            // 
+            this.YoutLabel.AutoSize = true;
+            this.YoutLabel.Location = new System.Drawing.Point(821, 225);
+            this.YoutLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.YoutLabel.Name = "YoutLabel";
+            this.YoutLabel.Size = new System.Drawing.Size(35, 13);
+            this.YoutLabel.TabIndex = 9;
+            this.YoutLabel.Text = "label2";
+            // 
+            // X0W0_label
+            // 
+            this.X0W0_label.AutoSize = true;
+            this.X0W0_label.Location = new System.Drawing.Point(265, 84);
+            this.X0W0_label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.X0W0_label.Name = "X0W0_label";
+            this.X0W0_label.Size = new System.Drawing.Size(35, 13);
+            this.X0W0_label.TabIndex = 10;
+            this.X0W0_label.Text = "label2";
+            // 
+            // X1W0_label
+            // 
+            this.X1W0_label.AutoSize = true;
+            this.X1W0_label.Location = new System.Drawing.Point(172, 291);
+            this.X1W0_label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.X1W0_label.Name = "X1W0_label";
+            this.X1W0_label.Size = new System.Drawing.Size(35, 13);
+            this.X1W0_label.TabIndex = 11;
+            this.X1W0_label.Text = "label2";
+            // 
+            // H1W0_Label
+            // 
+            this.H1W0_Label.AutoSize = true;
+            this.H1W0_Label.Location = new System.Drawing.Point(591, 350);
+            this.H1W0_Label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.H1W0_Label.Name = "H1W0_Label";
+            this.H1W0_Label.Size = new System.Drawing.Size(35, 13);
+            this.H1W0_Label.TabIndex = 12;
+            this.H1W0_Label.Text = "label2";
+            // 
+            // X0W1_label
+            // 
+            this.X0W1_label.AutoSize = true;
+            this.X0W1_label.Location = new System.Drawing.Point(190, 175);
+            this.X0W1_label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.X0W1_label.Name = "X0W1_label";
+            this.X0W1_label.Size = new System.Drawing.Size(35, 13);
+            this.X0W1_label.TabIndex = 13;
+            this.X0W1_label.Text = "label2";
+            // 
+            // H0W0_Label
+            // 
+            this.H0W0_Label.AutoSize = true;
+            this.H0W0_Label.Location = new System.Drawing.Point(591, 122);
+            this.H0W0_Label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.H0W0_Label.Name = "H0W0_Label";
+            this.H0W0_Label.Size = new System.Drawing.Size(35, 13);
+            this.H0W0_Label.TabIndex = 14;
+            this.H0W0_Label.Text = "label2";
+            // 
+            // H1outLabel
+            // 
+            this.H1outLabel.AutoSize = true;
+            this.H1outLabel.Location = new System.Drawing.Point(418, 390);
+            this.H1outLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.H1outLabel.Name = "H1outLabel";
+            this.H1outLabel.Size = new System.Drawing.Size(35, 13);
+            this.H1outLabel.TabIndex = 15;
+            this.H1outLabel.Text = "label2";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(219, 501);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(92, 29);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Antrenare";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Prag_H0
+            // 
+            this.Prag_H0.AutoSize = true;
+            this.Prag_H0.Location = new System.Drawing.Point(418, 25);
+            this.Prag_H0.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Prag_H0.Name = "Prag_H0";
+            this.Prag_H0.Size = new System.Drawing.Size(35, 13);
+            this.Prag_H0.TabIndex = 17;
+            this.Prag_H0.Text = "label2";
+            // 
+            // Prag_H1
+            // 
+            this.Prag_H1.AutoSize = true;
+            this.Prag_H1.Location = new System.Drawing.Point(418, 312);
+            this.Prag_H1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Prag_H1.Name = "Prag_H1";
+            this.Prag_H1.Size = new System.Drawing.Size(35, 13);
+            this.Prag_H1.TabIndex = 18;
+            this.Prag_H1.Text = "label2";
+            // 
+            // Prag_Y
+            // 
+            this.Prag_Y.AutoSize = true;
+            this.Prag_Y.Location = new System.Drawing.Point(821, 143);
+            this.Prag_Y.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Prag_Y.Name = "Prag_Y";
+            this.Prag_Y.Size = new System.Drawing.Size(35, 13);
+            this.Prag_Y.TabIndex = 19;
+            this.Prag_Y.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(954, 541);
+            this.Controls.Add(this.Prag_Y);
+            this.Controls.Add(this.Prag_H1);
+            this.Controls.Add(this.Prag_H0);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.H1outLabel);
+            this.Controls.Add(this.H0W0_Label);
+            this.Controls.Add(this.X0W1_label);
+            this.Controls.Add(this.H1W0_Label);
+            this.Controls.Add(this.X1W0_label);
+            this.Controls.Add(this.X0W0_label);
+            this.Controls.Add(this.YoutLabel);
+            this.Controls.Add(this.X1W1_label);
+            this.Controls.Add(this.H0outLabel);
+            this.Controls.Add(this.X1outLabel);
+            this.Controls.Add(this.X0outLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.panel1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label X0outLabel;
+        private System.Windows.Forms.Label X1outLabel;
+        private System.Windows.Forms.Label H0outLabel;
+        private System.Windows.Forms.Label X1W1_label;
+        private System.Windows.Forms.Label YoutLabel;
+        private System.Windows.Forms.Label X0W0_label;
+        private System.Windows.Forms.Label X1W0_label;
+        private System.Windows.Forms.Label H1W0_Label;
+        private System.Windows.Forms.Label X0W1_label;
+        private System.Windows.Forms.Label H0W0_Label;
+        private System.Windows.Forms.Label H1outLabel;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label Prag_H0;
+        private System.Windows.Forms.Label Prag_H1;
+        private System.Windows.Forms.Label Prag_Y;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace XORbp
+{
+    public partial class Form1 : Form
+    {
+        public class neuron
+        {
+            public double[] W { get; set; }
+            public double Out { get; set; }
+            public double prag { get; set; }
+            public neuron(int nr_elemente)
+            {
+                W = new double[nr_elemente];
+                for(int i = 0; i < nr_elemente; i++)
+                {
+                    W[i] = getRandomDouble();
+                }
+                prag = getRandomDouble();
+                Out = 0;
+            }
+        }
+        #region generareRandom
+        public static readonly Random getRandom = new Random();
+        public static int getRandomNumber(int min, int max)
+        {
+            lock (getRandom)
+            {
+                return getRandom.Next(min, max);
+            }
+        }
+        public static double getRandomDouble()
+        {
+            lock (getRandom)
+            {
+                return getRandom.NextDouble();
+            }
+        }
+        #endregion
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        #region variabile Globale
+        #endregion
+        private void EtapaAntrenare()
+        {
+           int nr_unitati_intrare = 2, hidden_neurons = 2;
+            double gradient = 0.5;
+            double E;
+            neuron[] x = new neuron[nr_unitati_intrare];
+            neuron y = new neuron(0);
+            neuron[] h = new neuron[hidden_neurons];
+            x[0] = new neuron(hidden_neurons);
+            x[1] = new neuron(hidden_neurons);
+            h[0] = new neuron(1);
+            h[1] = new neuron(1);
+            int k = 0;
+            int nr_exemple = 4;
+            double[] dateX1 = new double[nr_exemple];
+            double[] dateX2 = new double[nr_exemple];
+            double[] dateY = new double[nr_exemple];
+            string path = "input.txt";
+            string[] lines = File.ReadAllLines(path);
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string[] words = lines[i].Split(' ');
+                dateX1[i] = Convert.ToDouble(words[0]);
+                dateX2[i] = Convert.ToDouble(words[1]);
+              dateY[i] = Convert.ToDouble(words[2]);
+            }
+            double E_epoca = 0;
+            int epoca = 0;
+            do
+            {
+                epoca++;
+
+                label1.Text = $"Eroare: {E_epoca} \nEpoca: {epoca}";
+                label1.Update();
+           
+                
+              E_epoca = 0;
+                for (k = 0; k < nr_exemple; k++)
+                {
+                    x[0].Out = dateX1[k];
+                    x[1].Out = dateX2[k];
+                  #region labels
+                    
+                    X0outLabel.Text = x[0].Out.ToString();
+                    X1outLabel.Text = x[1].Out.ToString();
+                    H0outLabel.Text = h[0].Out.ToString();
+                    H1outLabel.Text = h[1].Out.ToString();
+                    YoutLabel.Text = y.Out.ToString();
+                    X0W0_label.Text = x[0].W[0].ToString();
+                    X0W1_label.Text = x[0].W[1].ToString();
+                    X1W1_label.Text = x[1].W[1].ToString();
+                    X1W0_label.Text = x[1].W[0].ToString();
+                    H0W0_Label.Text = h[0].W[0].ToString();
+                    H1W0_Label.Text = h[1].W[0].ToString();
+                    Prag_H0.Text = h[0].prag.ToString();
+                    Prag_H1.Text = h[1].prag.ToString();
+                    Prag_Y.Text = y.prag.ToString();
+                    H0W0_Label.Update();
+                    H1W0_Label.Update();
+                    X0outLabel.Update();
+                    X1outLabel.Update();
+                    H0outLabel.Update();
+                    H1outLabel.Update();
+                    panel1.Update();
+                    YoutLabel.Update();
+                    X0W0_label.Update();
+                    X0W1_label.Update();
+                    X1W0_label.Update();
+                    X1W1_label.Update();
+                    Prag_H0.Update();
+                    Prag_H1.Update();
+                    Prag_Y.Update();
+
+                    #endregion
+
+
+
+                   y.Out = 0;
+                    for (int a = 0; a < hidden_neurons; a++)
+                    {
+                       h[a].Out = 0;
+                        for (int i = 0; i < nr_unitati_intrare; i++)
+                        {
+                            h[a].Out += x[i].W[a] * x[i].Out + h[a].prag;
+                        }
+                        h[a].Out = F(h[a].Out);
+                        y.Out += h[a].W[0] * h[a].Out + y.prag;
+                    }
+                    y.Out = F(y.Out);
+                    E = Math.Pow((y.Out - dateY[k]), 2);
+                    E_epoca += E;
+
+                   
+
+                    double rez = 2 * (y.Out - dateY[k]) * F_derivat(y.Out);
+                    for (int a = 0; a < hidden_neurons; a++)
+                    {
+                        for (int i = 0; i < nr_unitati_intrare; i++)
+                        {
+                            x[i].W[a] = x[i].W[a] - (gradient * rez * h[a].W[0] * F_derivat(h[a].Out) * x[i].Out);
+                        }
+                        h[a].prag -= gradient * (rez) * h[a].W[0] * F_derivat(h[a].Out);
+                        h[a].W[0] -= (gradient * rez * h[a].Out);
+                    }
+                    y.prag -= (gradient * rez);
+                }
+                //Thread.Sleep(2000);
+            } while (E_epoca>Math.Pow(10,-3));
+          
+
+     
+
+
+        }
+        public double F(double x)
+        {
+            return 1.0 / (1.0 + Math.Exp(-x));
+        }
+        public double F_derivat(double x)
+        {
+            double rez = (x * (1 - x));
+            return rez;
+        }
+       
+        private void button2_Click(object sender, EventArgs e)
+        {
+            EtapaAntrenare();
+        }
+    
+    }
+}
